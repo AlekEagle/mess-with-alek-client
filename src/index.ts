9e59421397eded57181b42e0f6db1d62599c24bb
Alek Evans <gpg@alekeagle.com>
2022-04-13 14:13:36 -0600

    reconnect on disconnect

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import WS from 'ws';
-import { config } from 'dotenv';
-import { exec } from 'child_process';
-import { hostname } from 'os';
-import wrap from 'word-wrap';
+import WS from "ws";
+import { config } from "dotenv";
+import { exec } from "child_process";
+import { hostname } from "os";
+import wrap from "word-wrap";
 
 config();
 
@@ -29,14 +29,14 @@ enum ServerCloseCodes {
   InvalidToken = 4001,
   InvalidPayload = 4002,
   MissedHeartbeat = 4003,
-  AlreadyIdentified = 4004
+  AlreadyIdentified = 4004,
 }
 
 enum ClientCloseCodes {
   OK = 1000,
   ClientError = 4000,
   InvalidPayload = 4001,
-  MissedHeartbeat = 4002
+  MissedHeartbeat = 4002,
 }
 
 interface ClientPayloads {
@@ -55,61 +55,78 @@ interface GenericPayload {
   d: any;
 }
 
-const client = new WS(
-  process.env.DEBUG === 'true'
-    ? 'ws://localhost:3000/client'
-    : 'wss://dumb-alek.alekeagle.com/client'
-);
+let heartbeatTimeout: NodeJS.Timeout;
 
-client.on('open', () => {
-  if (process.env.DEBUG === 'true') console.log('Connected');
-  client.on('message', (data: string) => {
-    if (process.env.DEBUG === 'true') console.log(data.toString());
-    const payload: GenericPayload = JSON.parse(data.toString());
-    if (payload.op === 'IDENTIFY') {
-      client.send(
-        JSON.stringify({
-          op: 'IDENTITY',
-          d: {
-            token: process.env.TOKEN,
-            name: hostname()
-          }
-        })
-      );
-    } else if (payload.op === 'HEARTBEAT') {
-      client.send(
-        JSON.stringify({
-          op: 'HEARTBEAT',
-          d: {
-            timestamp: Date.now()
-          }
-        })
-      );
-      if (process.env.DEBUG === 'true') console.log('Heartbeat sent');
-    } else if (payload.op === 'IDENTIFIED') {
-      client.send(
-        JSON.stringify({
-          op: 'HEARTBEAT',
-          d: {
-            timestamp: Date.now()
-          }
-        })
-      );
-    } else if (payload.op === 'MESSAGE') {
-      const message = JSON.stringify(
-        wrap(payload.d, {
-          width: 50,
-          newline: '\n',
-          indent: '',
-          trim: true
-        })
-      );
-      console.log('Message received:', message);
-      exec(
-        `kdialog --title "Hivemind Communication Tunnel" --msgbox ${message}`
-      );
-    } else {
-      console.log('Unknown payload type:', payload.op);
-    }
+let client: WS;
+
+function createClient() {
+  client = new WS(
+    process.env.DEBUG === "true"
+      ? "ws://localhost:3000/client"
+      : "wss://dumb-alek.alekeagle.com/client"
+  );
+  client.on("open", () => {
+    if (process.env.DEBUG === "true") console.log("Connected");
+    client.on("message", (data: string) => {
+      if (process.env.DEBUG === "true") console.log(data.toString());
+      const payload: GenericPayload = JSON.parse(data.toString());
+      if (payload.op === "IDENTIFY") {
+        client.send(
+          JSON.stringify({
+            op: "IDENTITY",
+            d: {
+              token: process.env.TOKEN,
+              name: hostname(),
+            },
+          })
+        );
+      } else if (payload.op === "HEARTBEAT") {
+        clearTimeout(heartbeatTimeout);
+        client.send(
+          JSON.stringify({
+            op: "HEARTBEAT",
+            d: {
+              timestamp: Date.now(),
+            },
+          })
+        );
+        heartbeatTimeout = setTimeout(() => {
+          client.close(ClientCloseCodes.MissedHeartbeat);
+        }, payload.d.heartbeatInterval);
+        if (process.env.DEBUG === "true") console.log("Heartbeat sent");
+      } else if (payload.op === "IDENTIFIED") {
+        client.send(
+          JSON.stringify({
+            op: "HEARTBEAT",
+            d: {
+              timestamp: Date.now(),
+            },
+          })
+        );
+      } else if (payload.op === "MESSAGE") {
+        const message = JSON.stringify(
+          wrap(payload.d, {
+            width: 50,
+            newline: "\n",
+            indent: "",
+            trim: true,
+          })
+        );
+        console.log("Message received:", message);
+        exec(
+          `kdialog --title "Hivemind Communication Tunnel" --msgbox ${message}`
+        );
+      } else {
+        console.log("Unknown payload type:", payload.op);
+      }
+    });
   });
-});
+
+  client.on("close", (code: number) => {
+    console.log("Disconnected with code:", code);
+    console.log("Attempting to reconnect...");
+    setTimeout(createClient, 1000);
+  });
+}
+
+createClient();
